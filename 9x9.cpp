97541cb52de5c776a2e39f1821b2343fe89a646c
louisludev <louis.lu.company@gmail.com>
2025-08-22 22:50:58 +0800

    9x9=?

--- 9x9.cpp
+++ 9x9.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-  cout << "以下將列印出九九乘法表!" << "\n" << endl;
   for (int i = 1; i <= 9; i++) {
     for (int j = 1; j <= 9; j++) {
       cout << i << "x" << j << "=" << i * j << "\t";
     }
-    cout << endl;
+    cout << "\n";
   }
-  cout << "\n運算結束!" << endl;
-  return 0;
 }
